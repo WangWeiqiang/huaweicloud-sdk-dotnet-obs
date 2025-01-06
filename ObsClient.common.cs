61fa289b12b57f98e0868a63a9ce62e576bf6200
Wang Weiqiang <wangwq_net@hotmail.com>
2025-01-06 09:16:59 +0800

    Update ObsClient.common.cs
    update function PrepareHttpRequest

--- ObsClient.common.cs
+++ ObsClient.common.cs
@@ -1,4 +1,4 @@
-﻿/*----------------------------------------------------------------------------------
+/*----------------------------------------------------------------------------------
 // Copyright 2019 Huawei Technologies Co.,Ltd.
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License.  You may obtain a copy of the
@@ -307,6 +307,7 @@ namespace OBS
             }
             httpRequest.Endpoint = this.ObsConfig.Endpoint;
             httpRequest.PathStyle = this.ObsConfig.PathStyle;
+            httpRequest.UseCustomDomain = this.ObsConfig.UseCustomDomain;
             return httpRequest;
         }
 
